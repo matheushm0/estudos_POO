ac8327042de13f82143109846667cd39d1163de6
Matheus Holanda Matos <matheus-holanda@hotmail.com>
2021-10-05 10:18:51 -0300

    Add UrnaEletronica

---
+++ UrnaEletronica/UrnaEletronica/src/model/Candidato.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Candidato{
+   private int idCandidato;
+   private String nomeCandidato;
+   private String numeroCandidato;
+   private int votos;
+   private String idPartido;
+     
+    public int getIdCandidato() {
+        return idCandidato;
+    }
+
+    public void setIdCandidato(int idCandidato) {
+        this.idCandidato = idCandidato;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getNumeroCandidato() {
+        return numeroCandidato;
+    }
+
+    public void setNumeroCandidato(String numeroCandidato) {
+        this.numeroCandidato = numeroCandidato;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    } 
+
+    public String getIdPartido() {
+        return idPartido;
+    }
+
+    public void setIdPartido(String idPartido) {
+        this.idPartido = idPartido;
+    }
+}
